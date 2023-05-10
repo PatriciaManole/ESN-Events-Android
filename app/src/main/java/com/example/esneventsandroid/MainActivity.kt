78ee8ff9890cc4e49fbe92c929aa627bedffb8ca
PatriciaManole <patricia.manole@s.unibuc.ro>
2023-05-10 21:16:47 +0300

    Initial commit

---
+++ app/src/main/java/com/example/esneventsandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.esneventsandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_national_events)
+    }
+}
